d0149cc8e0653c12edc2d2a1a4be3777551870ae
R-oxy <aronmangati@gmail.com>
2023-02-16 12:25:04 -0800

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
-/**
-   * main - computes sizes of various data types
-   *
-   *Return: zero
-   */
-int main(void)
-{
-  	char c;
-	int i;
-	float f;
 
-	printf("Size of a char: %lu byte\(s\)\n", sizeof(c));
-	printf("Size of an int: %lu byte\(s\)\n", sizeof(i));
-	printf("Size of a long int: %lu byte\(s\)\n", sizeof(long int));
-	printf("Size of a long long int: %lu byte\(s\)\n", sizeof(long long int));
-	printf("Size of a float: %lu byte\(s\)\n", sizeof(f));
-	return (0);
+int main()
+{
+    printf("The size of char is %ld bytes\n", sizeof(char));
+    printf("The size of short is %ld bytes\n", sizeof(short));
+    printf("The size of int is %ld bytes\n", sizeof(int));
+    printf("The size of long is %ld bytes\n", sizeof(long));
+    printf("The size of long long is %ld bytes\n", sizeof(long long));
+    printf("The size of float is %ld bytes\n", sizeof(float));
+    printf("The size of double is %ld bytes\n", sizeof(double));
+    printf("The size of long double is %ld bytes\n", sizeof(long double));
+    printf("The size of void pointer is %ld bytes\n", sizeof(void*));
+    return 0;
 }
