a3f719952ca314f2a796669f125104832d2e40a5
R-oxy <aronmangati@gmail.com>
2023-02-21 05:48:24 -0800

    No spaces

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- *Main - prints the alphabet
+ *print_alphabet - prints the alphabet in lower case
  *
  *Return: void
  */
 void print_alphabet(void)
 {
-  char c;
+char c;
 
-  for(c = 'a'; c <= 'z'; c++)
-    {
-      _putchar(c);
-    }
+for(c = 'a'; c <= 'z'; c++)
+  {
+    _putchar(c);
+  }
 
-  _putchar('\n');
+ _putchar('\n');
 
 }
