afcf1b2ef624107be3dedf417551221cdd8b50fc
R-oxy <aronmangati@gmail.com>
2023-02-27 03:14:33 -0800

    Adds a program that reverses a string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,24 +2,24 @@
 #include <stdio.h>
 
 /**
- * print_rev - prints a string in reverse, followed by a newline character
- * @s: the string to print
- *
- *Return: void
- */
+  * print_rev - prints a string in reverse, followed by a newline character
+  * @s: the string to print
+  *
+  *Return: void
+  */
 void print_rev(char *s)
 {
-  int i, len = 0;
+        int i, len = 0;
 
-  while (s[len] != '\0')
-    {
-      len++;
-    }
+	while (s[len] != '\0')
+	{
+	        len++;
+	}
 
-  for (i = len - 1; i >= 0; i--)
-    {
-      putchar(s[i]);
-    }
+	for (i = len - 1; i >= 0; i--)
+	{
+	        putchar(s[i]);
+	}
 
-  putchar('\n');
+	putchar('\n');
 }
