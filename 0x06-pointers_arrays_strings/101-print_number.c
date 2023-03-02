ad2c777c70170b8590b47523a78694fcfc79ea5c
R-oxy <aronmangati@gmail.com>
2023-03-01 22:34:22 -0800

    Adds a program that prints an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer using only _putchar
+ * @n: the integer to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	int divisor = 1;
+
+	if (n < 0)
+	{
+        	_putchar('-');
+        	n = -n;
+	}
+
+	while (n / divisor >= 10)
+	{
+        	divisor *= 10;
+	}
+
+	while (divisor != 0) {
+        	_putchar('0' + n / divisor);
+        	n %= divisor;
+        	divisor /= 10;
+	}
+}
