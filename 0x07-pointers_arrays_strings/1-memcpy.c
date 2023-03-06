ac30eb50385c1be045bda849a5d3fc104c8b0757
R-oxy <aronmangati@gmail.com>
2023-03-05 23:30:14 -0800

    Adds a program that copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * Copies n bytes from memory area src to memory area dest.
+ *
+ * @param dest Pointer to the memory area to copy the bytes to.
+ * @param src Pointer to the memory area to copy the bytes from.
+ * @param n The number of bytes to be copied.
+ *
+ * @return Pointer to the memory area dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned char *p_dest = (unsigned char *) dest;
+	unsigned char *p_src = (unsigned char *) src;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*p_dest++ = *p_src++;
+	}
+
+	return (dest);
+}
