950430749569c321d08b71821ffcb38730901806
R-oxy <aronmangati@gmail.com>
2023-03-08 03:35:05 -0800

    Fixes 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,50 +7,22 @@
  *
  * Return: 1 if s1 and s2 are identical, 0 otherwise
  */
+int wildcmp(char* s1, char* s2) {
+    if (*s1 == '\0' && *s2 == '\0') {
+        return 1;
+    }
 
-int wildcmp(char *s1, char *s2)
-{
-	if (*s2 == '\0')
-	{
-		return (*s1 == '\0');
-	}
+    if (*s2 == '*') {
+        return wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2));
+    }
 
-	if (*s2 == '*')
-	{
-		while (*s2 == '*')
-		{
-			s2++;
-		}
+    if (*s1 == '\0' || *s2 == '\0') {
+        return 0;
+    }
 
-		if (*s2 == '\0')
-		{
-			return 1;
-		}
+    if (*s1 == *s2) {
+        return wildcmp(s1 + 1, s2 + 1);
+    }
 
-		while (*s1 != '\0' && *s2 != '\0')
-		{
-			if (*s1 == *s2 || *s2 == '?')
-			{
-				if (wildcmp(s1 + 1, s2 + 1))
-				{
-					return 1;
-				}
-			}
-
-			s1++;
-		}
-
-		return 0;
-	}
-	else
-	{
-		if (*s1 == *s2 || *s2 == '?')
-		{
-			if (wildcmp(s1 + 1, s2 + 1))
-			{
-				return 1;
-			}
-		}
-		return 0;
-	}
+    return 0;
 }
