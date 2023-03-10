d162a74215d1805f1c795075df6d09051baa2085
R-oxy <aronmangati@gmail.com>
2023-03-10 02:39:36 -0800

    Fixes 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,48 +1,65 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * main - Calculates the minimum number of coins needed to make change
- * @argc: Number of arguments passed to the program
- * @argv: Array of strings containing the arguments passed to the program
+ * main - prints the minimum number of coins to make change
+ * for an amount of money
  *
- * Return: 0 on success, 1 on failure
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 on success, 1 on error
  */
-int main(int argc, char *argv[]) {
-    int cents, coins = 0;
+int main(int argc, char *argv[])
+{
+        int cents, coins = 0;
 
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
-
-    cents = atoi(argv[1]);
-
-    if (cents < 0) {
-        printf("0\n");
-        return 0;
-    }
-
-    while (cents > 0) {
-        if (cents >= 25) {
-            coins += cents / 25;
-            cents %= 25;
-        } else if (cents >= 10) {
-            coins += cents / 10;
-            cents %= 10;
-        } else if (cents >= 5) {
-            coins += cents / 5;
-            cents %= 5;
-        } else if (cents >= 2) {
-            coins += cents / 2;
-            cents %= 2;
-        } else {
-            coins += cents;
-            cents = 0;
+        if (argc != 2)
+        {
+                printf("Error\n");
+                return (1);
         }
-    }
 
-    printf("%d\n", coins);
-    return 0;
+        cents = atoi(argv[1]);
+
+        if (cents < 0)
+        {
+                printf("0\n");
+                return (0);
+        }
+
+        while (cents >= 25)
+        {
+                coins++;
+                cents -= 25;
+        }
+
+        while (cents >= 10)
+        {
+                coins++;
+                cents -= 10;
+        }
+
+        while (cents >= 5)
+        {
+                coins++;
+                cents -= 5;
+        }
+
+        while (cents >= 2)
+        {
+                coins++;
+                cents -= 2;
+        }
+
+        while (cents >= 1)
+        {
+                coins++;
+                cents -= 1;
+        }
+
+        printf("%d\n", coins);
+
+        return (0);
 }
