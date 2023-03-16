105f3119579d20f74ef7435a65a69f6b9a7c2428
R-oxy <aronmangati@gmail.com>
2023-03-16 03:00:33 -0700

    Fixes the program that allocates memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -6,16 +6,16 @@
  * malloc_checked - Allocate memory using malloc.
  * b: unsigned int - The number of bytes to allocate.
  *
- * Return:void* - A pointer to the allocated memory.
+ * Return: void* - A pointer to the allocated memory.
  */
 void *malloc_checked(unsigned int b)
 {
-	void *p;
+	void *ptr = malloc(b);
 
-	p = malloc(b);
-
-	if (p == NULL)
+	if (ptr == NULL)
+	{
 		exit(98);
+	}
 
-	return (p);
+	return (ptr);
 }
