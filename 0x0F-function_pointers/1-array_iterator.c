db54bbca4e9a2d0cd76164301af38726c59ddc01
R-oxy <aronmangati@gmail.com>
2023-03-22 05:55:17 -0700

    Fixes program that executes a function given as a parameter on each element of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,12 +11,14 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i = 0;
-	if (array != NULL && action != NULL)
+	unsigned int i = 0;
+
+	if (array != NULL && action != NULL && size > 0)
 	{
-		for (i = 0; i < size; i++)
+		while (i < size)
 		{
 			action(array[i]);
+			i++;
 		}
 	}
 }
