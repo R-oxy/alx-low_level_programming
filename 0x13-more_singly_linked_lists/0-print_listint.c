519df21fc77b76d027824b27d851f948447ca141
R-oxy <aronmangati@gmail.com>
2023-04-04 00:44:05 -0700

    Adds a program that adds a new node at the beginning of a listint_t list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,13 +3,13 @@
 
 /**
   * print_listint - Prints all elements of listint_t list.
-  * @h: pointer to the head of listint_t list
+  * @h: pointer to the head of listint_t list.
   *
-  * Return: number of nodes in listint_t list
+  * Return: number of nodes in listint_t list.
   */
 size_t print_listint(const listint_t *h)
 {
-	int node_count = 0;
+	size_t count = 0;
 
 	if (h != NULL)
 	{
@@ -17,9 +17,9 @@ size_t print_listint(const listint_t *h)
 		{
 			printf("%d\n", h->n);
 			h = h->next;
-			node_count++;
+			count++;
 		}
 	}
 
-	return (node_count);
+	return (count);
 }
