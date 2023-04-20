ad6b5024cfb6d376df1bc7e11a915f49da3b5765
R-oxy <aronmangati@gmail.com>
2023-04-19 23:39:26 -0700

    Fixed bugs in main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,9 +11,9 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-static int *open_files(const char *file_from, const char *file_to)
-static int copy_data(int fd_from, int fd_to)
-static void close_files(int *fds)
-int copy_file(const char *file_from, const char *file_to)
-  
+static int *open_files(const char *file_from, const char *file_to);
+static int copy_data(int fd_from, int fd_to);
+static void close_files(int *fds);
+int copy_file(const char *file_from, const char *file_to);
+
 #endif
