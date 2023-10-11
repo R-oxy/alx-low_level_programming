a97be59a2d611f5397748e3139cff960f4376742
R-oxy <aronmangati@gmail.com>
2023-10-11 16:37:54 +0300

    Adds a function that searches for a value in an array of integers using the Linear search algorithm

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,6 +4,35 @@
 /* Standard Libraries */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+
+/* Structures */
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ * @n: Integer stored in the node
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ */
+typedef struct skiplist_s
+{
+    int n;
+    struct skiplist_s *next;
+    struct skiplist_s *express;
+} skiplist_t;
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for ALX project
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
 
 /* Function Prototypes */
 int linear_search(int *array, size_t size, int value);
