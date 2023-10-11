0be3a503deeba946f578532eb25e3e2b4aafebb5
R-oxy <aronmangati@gmail.com>
2023-10-11 20:50:41 +0300

    Adds a function that searches for a value in a sorted array of integers using the Exponential search algorithm

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,78 @@
+#include "search_algos.h"
+
+/**
+* binary_search - searches for a value in a sorted array of integers
+* using the Binary search algorithm
+* @array: a pointer to the first element of the array to search in
+* @size: the number of elements in array
+* @value: the value to search for
+* Return: the index where value is located, or -1 if it's not present
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	int left = 0;
+
+	int right = size - 1;
+
+	int mid;
+
+	int i;
+
+	if (array == NULL)
+		return (-1);
+
+	while (left <= right)
+	{
+		mid = left + (right - left) / 2;
+
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			if (i == right)
+				printf("%d\n", array[i]);
+			else
+				printf("%d, ", array[i]);
+		}
+
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+
+	return (-1);
+}
+
+/**
+* exponential_search - Searches for a value in a sorted array of integers
+*                      using the Exponential search algorithm.
+* @array: A pointer to the first element of the array to search in.
+* @size: The number of elements in the array.
+* @value: The value to search for.
+*
+* Return: The first index where the value is located, or -1 if the value
+*         is not present in the array or if the array is NULL.
+*/
+int exponential_search(int *array, size_t size, int value)
+{
+	size_t i = 1;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	while (i < size && array[i] <= value)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		i *= 2;
+	}
+
+	printf(
+		"Value found between indexes [%lu] and [%lu]\n"
+		, i / 2, i >= size ? size - 1 : i);
+
+	return (binary_search(array, i >= size ? size : i + 1, value));
+}
